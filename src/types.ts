a2bb91c0889f4a5f8392ec78876d5e86519e4cc9
João Azevedo <joaoazevedo@outlook.com>
2021-02-28 15:57:44 +0000

    Split into two interfaces, data structure from data utils

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type Types = "percentage" | "number" | "secs" | "hours";
 
-export interface Metric {
+interface DataStructure {
   id: string;
   label: string;
   value: number;
@@ -11,7 +11,11 @@ export interface Metric {
 }
 
 export interface ApiData {
-  data: Metric[];
+  data: DataStructure[];
+}
+
+export interface Metric extends DataStructure {
+  show?: boolean;
 }
 
 export type SortState = "asc" | "desc";
